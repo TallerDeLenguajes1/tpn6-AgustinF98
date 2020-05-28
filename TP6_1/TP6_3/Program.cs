1302f485d39f09847f4eec751dd7f4418f9ce27f
Agustin <agus_gymnas2009@hotmail.com>
2020-05-28 10:56:11 -0300

    Agrego punto 3

--- TP6_1/TP6_3/Program.cs
+++ TP6_1/TP6_3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace TP6_3
 {
@@ -7,11 +6,34 @@ namespace TP6_3
     {
         static void Main(string[] args)
         {
-            float num1, num2, num3;
-            Console.WriteLine("--- CALCULADORA MEJORADA ---");
-            Console.WriteLine("Ingrese un numero: ");
-            num1 = ;
+            double num1, resultado = 0;
+            int num2, num3;
             
+            Console.WriteLine("--- CALCULADORA MEJORADA ---\n");
+            Console.WriteLine("Ingrese un numero con decimales: ");
+            num1 = Double.Parse(Console.ReadLine());
+
+            resultado = Math.Abs(num1);
+            Console.WriteLine("\nEl valor absoluto del numero: " + num1 + " es: " + resultado);
+            resultado = Math.Pow(num1, 2);
+            Console.WriteLine("El cuadrado del numero: " + num1 + " es: " + resultado);
+            resultado = Math.Sqrt(num1);
+            Console.WriteLine("La raiz cuadrada del numero: " + num1 + " es: " + resultado);
+            resultado = Math.Sin(num1);
+            Console.WriteLine("El seno del numero: " + num1 + " es: " + resultado);
+            resultado = Math.Cos(num1);
+            Console.WriteLine("El coseno del numero: " + num1 + " es: " + resultado);
+            resultado = Math.Truncate(num1);
+            Console.WriteLine("La parte entera del numero: " + num1 + " es: " + resultado);
+
+            Console.WriteLine("\nIngrese dos numeros enteros");
+            Console.WriteLine("Primer numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Segundo numero: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEl maximo de los 2 numeros ingresados es: " + Math.Max(num2, num3));
+            Console.WriteLine("El minimo de los 2 numeros ingresados es: " + Math.Min(num2, num3));
         }
     }
 }
